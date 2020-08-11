f89dd4b2896fc28b9884b3fd6445412e11c5fe70
c h <me@chicks06.co.uk>
2020-08-11 22:01:26 +0100

    PlayerController now checks whether collision is under bird (e.g. flying or walking)

--- Bird/Assets/Scripts/PlayerController.cs
+++ Bird/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,9 @@ public class PlayerController : MonoBehaviour
     public float MaxFlapSpeed = 5f;
     public float FlapIncrement = 0.005f;
     public float SlowFall = 0.3f;
+
+
+    [SerializeField] bool onFloor;
     [SerializeField] Vector3 currentVelocity;
 
 
@@ -31,14 +34,15 @@ public class PlayerController : MonoBehaviour
     private void ApplyMotionFromInput()
     {
         //forward
-
-        if (Rigidbody.velocity.z > MaxForwardSpeed)
+        float currentForwardMaxSpeed = (onFloor ? 0.5f : 1.0f) * MaxForwardSpeed;
+        float currentForwardForce = (onFloor ? 0.5f : 1.0f) * ForwardForce;
+        if (Rigidbody.velocity.z > currentForwardMaxSpeed)
         {
-            Rigidbody.AddForce(transform.forward * -ForwardForce);
+            Rigidbody.AddForce(transform.forward * -currentForwardForce);
         }
         else if (Input.GetKey(KeyCode.LeftShift))
         {
-            Rigidbody.AddForce(transform.forward * ForwardForce);
+            Rigidbody.AddForce(transform.forward * currentForwardForce);
         }
 
 
@@ -68,5 +72,32 @@ public class PlayerController : MonoBehaviour
         }
 
         currentVelocity = Rigidbody.velocity;
+    }
+
+    //Todo update animations for flying/walking
+    public void SetOnFloor(bool onFloor)
+    {
+        if (this.onFloor == onFloor)
+        {
+            return;
+        }
+
+        this.onFloor = onFloor;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contactPoint in collision.contacts)
+        {
+            if (contactPoint.normal == Vector3.up)
+            {
+                SetOnFloor(true);
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        SetOnFloor(false);
     }
 }
