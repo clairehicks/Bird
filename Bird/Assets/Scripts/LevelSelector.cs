ab2ffd2242e11a302386d16a83482ae561569842
c h <me@chicks06.co.uk>
2020-08-12 11:36:16 +0100

    Create Menu

---
+++ Bird/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private List<TMP_Dropdown.OptionData> allLevels;
+    [SerializeField] int currentMaxLevel;
+    TMP_Dropdown dropdown;
+
+    void Start()
+    {
+        //todo set player load/save
+        currentMaxLevel = PlayerData.GetCurrentMaxLevel();
+        dropdown = gameObject.GetComponent<TMP_Dropdown>();
+
+        SetDropdown();
+    }
+
+    private void SetDropdown()
+    {
+        dropdown.ClearOptions();
+
+        //setup all level names
+        allLevels = new List<TMP_Dropdown.OptionData>();
+        allLevels.Add(new TMP_Dropdown.OptionData("Level 1: Learn to fly"));
+        allLevels.Add(new TMP_Dropdown.OptionData("Level 2: Eating seed"));
+        allLevels.Add(new TMP_Dropdown.OptionData("Level 3: Open the door"));
+        allLevels.Add(new TMP_Dropdown.OptionData("Level 4: Time to wash"));
+        allLevels.Add(new TMP_Dropdown.OptionData("Level 5: Free to explore"));
+
+        for (int i = allLevels.Count-1; i >= currentMaxLevel ; i--)
+        {
+            allLevels.Remove(allLevels[i]);
+        }
+        dropdown.AddOptions(allLevels);
+
+        PlayerData.CurrentLevel = currentMaxLevel;
+        dropdown.SetValueWithoutNotify(currentMaxLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ResetProgress()
+    {
+        PlayerData.ClearData();
+        currentMaxLevel = PlayerData.GetCurrentMaxLevel();
+        SetDropdown();
+    }
+
+    public void LoadLevel()
+    {
+        PlayerData.CurrentLevel = dropdown.value + 1;
+        SceneManager.LoadScene("Main");
+    }
+}
---
+++ Bird/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+//Handles Player Info which is stored in PlayerPrefs
+public static class PlayerData
+{
+    public static int CurrentLevel; //this does not need saving
+    private const int MaxLevel = 5;
+    private const string CurrentMaxLevel = "CurrentMaxLevel";
+    //todo add high score and achievements
+
+    public static void LevelCompleted(int level)
+    {
+        if (level > GetCurrentMaxLevel() && level < MaxLevel)
+        {
+            PlayerPrefs.SetInt(CurrentMaxLevel, GetCurrentMaxLevel() + 1);
+        }
+    }
+
+
+    public static int GetCurrentMaxLevel()
+    {
+        if (!PlayerPrefs.HasKey(CurrentMaxLevel))
+        {
+            ClearData();
+        }
+        return PlayerPrefs.GetInt(CurrentMaxLevel);
+
+    }
+
+    public static void ClearData(){
+        PlayerPrefs.SetInt(CurrentMaxLevel, 1);
+    }
+}
