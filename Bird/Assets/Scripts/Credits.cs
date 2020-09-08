ff3bd3479e10cdad4e47239616ca4e8fa41f23cf
c h <me@chicks06.co.uk>
2020-09-09 00:38:43 +0100

    Added Credits

---
+++ Bird/Assets/Scripts/Credits.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GoToCredits()
+    {
+        Application.OpenURL("https://chicks06.co.uk/BirdCredits");
+    }
+}
